202a778f384247257d79ab6296eac82b53f4912a
khaledhosny129 <86989101+khaledhosny129@users.noreply.github.com>
2024-09-03 04:13:50 +0300

    Update errorController.js

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -14,7 +14,7 @@ const handleCastErrorDB = err => {
 
 const handleDuplicateFieldsDB = err => {
   const value = err.keyValue ? Object.values(err.keyValue)[0] : '';
-  const message = `Duplicate field value: ${value}. Please use another value!`;
+  const message = `This email is already exist: ${value}. Please use another email!`;
   return new AppError(message, 400);
 };
 

58b2fb018774f696ac2712063a0642fc2e19e9b4
khaledhosny129 <86989101+khaledhosny129@users.noreply.github.com>
2024-09-03 15:41:17 +0300

    Update errorController.js

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -8,7 +8,7 @@ const handleCastErrorDB = err => {
 // const handleDuplicateFieldsDB = err => {
 //   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
 
-//   const message = `This email is already exist: ${value}. Please use another email!`;
+//   const message = `Duplicate field value: ${value}. Please use another value!`;
 //   return new AppError(message, 400);
 // };
 
@@ -25,30 +25,33 @@ const handleValidationErrorDB = err => {
   return new AppError(message, 400);
 };
 
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack
-  });
+const sendErrorDev = (err, req, res) => {
+  if (req.originalUrl.startsWith('/api')) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack
+    });
+  }
 };
 
-const sendErrorProd = (err, res) => {
+const sendErrorProd = (err, req, res) => {
   // Operational, trusted error: send message to client
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message
-    });
+  if (req.originalUrl.startsWith('/api')) {
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+      });
 
-    // Programming or other unknown error: don't leak error details
-  } else {
+      // Programming or other unknown error: don't leak error details
+    }
     // 1) Log error
     console.error('ERROR 💥', err);
 
     // 2) Send generic message
-    res.status(500).json({
+    return res.status(500).json({
       status: 'error',
       message: 'Something went very wrong!'
     });
@@ -62,7 +65,7 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
+    sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
     error.message = err.message;
@@ -72,6 +75,6 @@ module.exports = (err, req, res, next) => {
     if (error.name === 'ValidationError')
       error = handleValidationErrorDB(error);
 
-    sendErrorProd(error, res);
+    sendErrorProd(error, req, res);
   }
 };

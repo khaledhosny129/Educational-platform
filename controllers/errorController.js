a439a47c60962ba3619e327327d89b3b4c9917a0
khaledhosny129 <86989101+khaledhosny129@users.noreply.github.com>
2024-09-03 15:51:07 +0300

    Update errorController.js

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,26 +1,25 @@
-const AppError = require('./../utils/appError');
+const AppError = require('../utils/appError');
+
+// Deep clone error object for production error handling
+const deepCloneError = (err) => {
+  const clonedError = JSON.parse(JSON.stringify(err));
+  clonedError.message = err.message;
+  return clonedError;
+};
 
 const handleCastErrorDB = err => {
   const message = `Invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
 };
 
-// const handleDuplicateFieldsDB = err => {
-//   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-
-//   const message = `Duplicate field value: ${value}. Please use another value!`;
-//   return new AppError(message, 400);
-// };
-
 const handleDuplicateFieldsDB = err => {
   const value = err.keyValue ? Object.values(err.keyValue)[0] : '';
-  const message = `This email is already exist: ${value}. Please use another email!`;
+  const message = `This email is already taken: ${value}. Please use another email!`;
   return new AppError(message, 400);
 };
 
 const handleValidationErrorDB = err => {
   const errors = Object.values(err.errors).map(el => el.message);
-
   const message = `Invalid input data. ${errors.join('. ')}`;
   return new AppError(message, 400);
 };
@@ -34,6 +33,7 @@ const sendErrorDev = (err, req, res) => {
       stack: err.stack
     });
   }
+  // Add code here if you're rendering HTML pages
 };
 
 const sendErrorProd = (err, req, res) => {
@@ -44,36 +44,29 @@ const sendErrorProd = (err, req, res) => {
         status: err.status,
         message: err.message
       });
-
-      // Programming or other unknown error: don't leak error details
     }
-    // 1) Log error
+    // Programming or other unknown error: don't leak error details
     console.error('ERROR 💥', err);
-
-    // 2) Send generic message
     return res.status(500).json({
       status: 'error',
       message: 'Something went very wrong!'
     });
   }
+  // Add code here if you're rendering HTML pages
 };
 
 module.exports = (err, req, res, next) => {
-  // console.log(err.stack);
-
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === 'production') {
-    let error = { ...err };
-    error.message = err.message;
+    let error = deepCloneError(err);
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (error.name === 'ValidationError')
-      error = handleValidationErrorDB(error);
+    if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
 
     sendErrorProd(error, req, res);
   }

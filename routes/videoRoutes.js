e976f4e2d4bd5543dba516c3bf7eba9f1c43160c
khaledhosny129 <86989101+khaledhosny129@users.noreply.github.com>
2024-09-13 16:32:36 +0300

    Update videoRoutes.js

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.use(authController.protect);
 
+// Normal video routes (with unit)
 router
   .route('/:grade/:level/:unit/:session')
   .post(authController.restrictTo('admin'), videoController.uploadVideo)
@@ -13,23 +14,35 @@ router
   .patch(authController.restrictTo('admin'), videoController.updateVideo)
   .delete(authController.restrictTo('admin'), videoController.deleteVideo);
 
+// Revision video routes (without unit)
 router
-  .route('/:grade/:level/revision/:num')
-  .post(authController.restrictTo('admin'), videoController.uploadVideo) // Reuse the same controller method
-  .get(videoController.getVideo) // Reuse the same controller method
-  .patch(authController.restrictTo('admin'), videoController.updateVideo) // Reuse the same controller method
-  .delete(authController.restrictTo('admin'), videoController.deleteVideo); // Reuse the same controller method
+  .route('/:grade/:level/r:revision/:session')
+  .post(authController.restrictTo('admin'), videoController.uploadVideo)
+  .get(videoController.getVideo)
+  .patch(authController.restrictTo('admin'), videoController.updateVideo)
+  .delete(authController.restrictTo('admin'), videoController.deleteVideo);
 
+// Activate video
 router
   .route('/:grade/:level/:unit/:session/activate')
   .post(videoController.activateVideo);
 
 router
+  .route('/:grade/:level/r:revision/:session/activate')
+  .post(videoController.activateVideo);
+
+// Deactivate video
+router
   .route('/:grade/:level/:unit/:session/deactivate')
   .post(videoController.deactivateVideo);
 
-router.route('/').get(videoController.getAllVideos);
+router
+  .route('/:grade/:level/r:revision/:session/deactivate')
+  .post(videoController.deactivateVideo);
 
-router.route('/activated').get(videoController.getActivatedVideos);
+// Get activated videos for a user
+router
+  .route('/activations')
+  .get(videoController.getActivatedVideos);
 
 module.exports = router;

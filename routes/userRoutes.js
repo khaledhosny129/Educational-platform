4cb0a94c6819306ba5f3841b55abdda656e73849
khaledhosny129 <86989101+khaledhosny129@users.noreply.github.com>
2024-09-18 17:09:05 +0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,7 +10,7 @@ router.post('/login', authController.login);
 router.get('/logout', authController.logout);
 router.post('/confirmEmail', authController.confirmEmail);
 router.post('/forgotPassword', authController.forgotPassword);
-router.patch('/resetPassword/:token', authController.resetPassword);
+router.patch('/resetPassword', authController.resetPassword);
 router.get('/verify/:token', authController.verifyEmail);
 router.get('/', userController.getAllUsers);
 
